cee4bcecbab437b686acb60ca9c394a28985a6e5
dzurrihensem <dzurripro@gmail.com>
2026-03-12 00:19:11 +0800

    Update firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,14 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// Kod rahsia Cikgu (Dah masukkan yang betul)
 const firebaseConfig = {
-  apiKey: "ISI_API_KEY_ANDA",
-  authDomain: "ISI_PROJECT_ID.firebaseapp.com",
-  projectId: "ISI_PROJECT_ID",
-  storageBucket: "ISI_PROJECT_ID.appspot.com",
-  messagingSenderId: "ISI_SENDER_ID",
-  appId: "ISI_APP_ID"
+  apiKey: "AIzaSyBfIJJtJ9pkv7FwQqd_QASeorxVLv7Wgrg",
+  authDomain: "smart-rph-dzurri.firebaseapp.com",
+  projectId: "smart-rph-dzurri",
+  storageBucket: "smart-rph-dzurri.firebasestorage.app",
+  messagingSenderId: "1067321929449",
+  appId: "1:1067321929449:web:db07762c316c9ed0464a78",
+  measurementId: "G-S3XQWYF07E"
 };
 
+// 1. Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// 2. Export database Firestore (Ini yang App.tsx cari!)
 export const db = getFirestore(app);
